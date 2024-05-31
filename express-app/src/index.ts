5602d65bc4dfbded47cb792814a87902df458f2f
Tejas <tejwagh@deloitte.com>
2024-05-31 14:22:48 +0530

    Fixed package.json

--- express-app/src/index.ts
+++ express-app/src/index.ts
@@ -1,24 +1,11 @@
 
 import express from "express";
-import { z } from "zod";
 
 export const app = express();
 app.use(express.json());
 
-// const sumInput = z.object({
-//     a: z.number(),
-//     b: z.number()
-// })
-
 app.post("/sum", (req, res) => {
-    // const parsedResponse = sumInput.safeParse(req.body)
-    
-    // if (!parsedResponse.success) {
-    //     return res.status(411).json({
-    //         message: "Incorrect inputs"
-    //     })
-    // }
-
+ 
     const answer = req.body.a + req.body.b;
 
     res.status(200).json({
